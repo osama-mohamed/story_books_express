4d666158e67c2abcc5b68834a9056bdc72e7414f
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-26 18:11:20 +0200

    create story

--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {ensureAuthenticated, ensureGuest} = require("../helpers/auth");
-
+const Story = require("../models/Story");
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
 router.get("/", (req, res) => {
   res.render("stories/index");
@@ -11,13 +11,34 @@ router.get("/show/:id", (req, res) => {
   res.render("stories/show");
 });
 
+
 router.get("/add", ensureAuthenticated, (req, res) => {
   res.render("stories/add");
 });
 
+router.post("/", ensureAuthenticated, (req, res) => {
+  let allowComments;
+  if (req.body.allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+  
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: allowComments,
+    user: req.user._id
+  };
+  new Story(newStory).save().then(story => {
+    res.redirect(`/stories/show/${story._id}`);
+  });
+});
+
+
 router.get("/edit/:id", ensureAuthenticated, (req, res) => {
   res.render("stories/edit");
 });
-
 
 module.exports = router;
