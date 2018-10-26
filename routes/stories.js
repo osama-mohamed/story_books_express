5df576a650a8de00590dc11aa7a798ad1ce520bb
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-26 22:38:20 +0200

    show private story by owner only

--- routes/stories.js
+++ routes/stories.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const Story = require("../models/Story");
 const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
-
 // stories of a user
 router.get("/user/:userId", (req, res) => {
-  Story.find({ user: req.params.userId, status: 'public' })
+  Story.find({ user: req.params.userId, status: "public" })
     .populate("user")
     .then(stories => {
       if (stories) {
@@ -26,12 +25,11 @@ router.get("/my", ensureAuthenticated, (req, res) => {
     });
 });
 
-
 // get all public stories
 router.get("/", (req, res) => {
   Story.find({ status: "public" })
     .populate("user")
-    .sort({date: 'desc'})
+    .sort({ date: "desc" })
     .then(stories => {
       res.render("stories/index", { stories: stories });
     });
@@ -43,10 +41,18 @@ router.get("/show/:id", (req, res) => {
     .populate("user")
     .populate("comments.commentUser")
     .then(story => {
-      if (story) {
+      if (story.status == "public") {
         res.render("stories/show", { story: story });
-        // } else {
-        //   res.render('stories/show', {story: story});
+      } else {
+        if (req.user) {
+          if (req.user.id == story.user._id) {
+            res.render("stories/show", { story: story });
+          } else {
+            res.redirect('/stories');
+          }
+        } else {
+          res.redirect('/stories');
+        }
       }
     });
 });
@@ -81,7 +87,7 @@ router.post("/", ensureAuthenticated, (req, res) => {
 router.get("/edit/:id", ensureAuthenticated, (req, res) => {
   Story.findOne({ _id: req.params.id }).then(story => {
     if (story.user != req.user.id) {
-      res.redirect('/stories');
+      res.redirect("/stories");
     } else {
       res.render("stories/edit", { story: story });
     }
@@ -99,10 +105,10 @@ router.put("/:id", ensureAuthenticated, (req, res) => {
         allowComments = false;
       }
 
-      story.title= req.body.title;
-      story.body= req.body.body;
-      story.status= req.body.status;
-      story.allowComments= allowComments;
+      story.title = req.body.title;
+      story.body = req.body.body;
+      story.status = req.body.status;
+      story.allowComments = allowComments;
       story.save().then(story => {
         res.redirect(`/stories/show/${story._id}`);
       });
@@ -126,7 +132,7 @@ router.post("/comment/:id", ensureAuthenticated, (req, res) => {
       const newComment = {
         commentBody: req.body.commentBody,
         commentUser: req.user.id
-      }
+      };
       story.comments.unshift(newComment);
       story.save().then(story => {
         if (story) {
